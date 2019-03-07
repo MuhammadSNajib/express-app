c66fed767102754e9025eaecc699010e0f57774f
Najib <muhammad.es.najib@gmail.com>
2019-03-07 18:00:26 +0700

    Handling Requests

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+//handling requests
+app.get("/api/products", function(req, res){
+  res.send({
+    name: "Coffee",
+    price: 12000
+  })
+});
+
 app.listen(process.env.port || 5000, function() {
-  console.log("Express app now listening requests");
+  console.log("Express app now listening requests 5000");
 });
